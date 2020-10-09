7b7b1bcf5bfaac2559801a1dff3f67630143f408
Angi Rubio <rubio.mailbox@gmail.com>
2020-10-09 14:37:03 +0200

    Routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { PerfilPropioComponent } from './pages/perfil-propio/perfil-propio.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {path: "", redirectTo: "/landing-page", pathMatch:'full'},
+  {path: "landing-page", component: LandingPageComponent},
+  {path: "home", component: HomeComponent},
+  {path: "perfil", component: PerfilPropioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
