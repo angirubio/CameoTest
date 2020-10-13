abc06295be61d2aee938b0ce0ae54224af72fd5b
pipe473 <bedoyafelipe743@gmail.com>
2020-10-13 11:01:39 +0200

    landing page enrutada y funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { PerfilPropioComponent } from './pages/perfil-propio/perfil-propio.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 const routes: Routes =
 [
   {path: "", redirectTo: "/landing-page", pathMatch:'full'},
   {path: "landing-page", component: LandingPageComponent},
   {path: "home", component: HomeComponent},
-  {path: "perfil", component: PerfilPropioComponent}
+  {path: "perfil", component: PerfilPropioComponent},
+  {path: "login", component: LoginComponent},
+  {path: "registro", component: RegistroComponent },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,185 +22,185 @@ export class AppComponent implements OnInit {
   constructor(@Inject(DOCUMENT) private document: Document){}
 
   ngOnInit(){
-    this.initialAnimations();
-    this.initScrollAnimations();
+    // this.initialAnimations();
+    // this.initScrollAnimations();
   }
 
-  initScrollAnimations(): void{
-    gsap.to(this.imageFirst.nativeElement, {
-      scrollTrigger: {
-        trigger: this.imageFirst.nativeElement,
-        scrub: true,
-        start: '110% center',
-      } as gsap.plugins.ScrollTriggerInstanceVars,
-      duration: 1.1,
-      scale: 1.2, 
-      height: 250,
-    });
-    gsap.to(this.imageSecond.nativeElement, {
-      scrollTrigger: {
-        trigger: this.imageSecond.nativeElement,
-        scrub: true,
-        start: '80% center'
-      },
-      duration: 1.1,
-      scale: 1.2,
-      height: 380,
-    });
-    gsap.to(this.document.querySelector('.heading-1'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('.heading-1'),
-        scrub: true,
-        start: '150% center',
-      },
-      color: '#fff',
-      duration: 1.5,
-    });
-    gsap.to(this.document.querySelector('.paragraph'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('.paragraph'),
-        scrub: true,
-        start: '150% center',
-      },
-      color: '#fff',
-      duration: 1.5
-    });
-    gsap.to(this.document.querySelector('.btn'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('btn'),
-        scrub: true,
-        start: '150% center',
-      },
-      color: '#fff',
-      duration: 1.5
-    });
-    gsap.from(this.document.querySelector('#buy'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('#buy'),
-        scrub: true,
-        toggleClass: 'active',
-        start: 'top center',
-      },
-      duration: 1.5,
-      y: 40,
-      opacity: 0,
-    });
-    gsap.from(this.document.querySelector('#about'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('#about'),
-        scrub: true,
-        toggleClass: 'active',
-        start: 'top center',
-      },
-      duration: 1.5,
-      y: 40,
-      opacity: 0,
-    });
-    gsap.from(this.document.querySelector('.box'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('.box'),
-        scrub: true,
-        toggleClass: 'active',
-        start: '-10% center',
-      },
-      duration: 1.5,
-      width: 0,
-      opacity: 0,
-    });
-    gsap.from(this.document.querySelector('.info-1__visual img'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('.info-1__visual img'),
-        scrub: true,
-        toggleClass: 'active',
-        start: '-60% bottom',
-      },
-      duration: 1.5,
-      width: 0,
-      opacity: 0,
-    });
-    gsap.from(this.document.querySelector('.quote'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('.quote'),
-        scrub: true,
-        toggleClass: 'active',
-        start: '-60% bottom',
-      },
-      duration: 1.5,
-      opacity: 0,
-    });
-    gsap.from(this.document.querySelector('.info-1__visual .heading-3'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('.info-1__visual .heading-3'),
-        scrub: true,
-        toggleClass: 'active',
-        start: '-60% bottom',
-      },
-      duration: 1.5,
-      y: 40,
-      opacity: 0,
-    });
-    gsap.from(this.document.querySelector('.info-1__visual .btn--learn'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('.info-1__visual .btn--learn'),
-        scrub: true,
-        toggleClass: 'active',
-        start: '-60% bottom',
-      },
-      duration: 1.5,
-      y: 40,
-      opacity: 0,
-    });
-  }
+  // initScrollAnimations(): void{
+  //   gsap.to(this.imageFirst.nativeElement, {
+  //     scrollTrigger: {
+  //       trigger: this.imageFirst.nativeElement,
+  //       scrub: true,
+  //       start: '110% center',
+  //     } as gsap.plugins.ScrollTriggerInstanceVars,
+  //     duration: 1.1,
+  //     scale: 1.2, 
+  //     height: 250,
+  //   });
+  //   gsap.to(this.imageSecond.nativeElement, {
+  //     scrollTrigger: {
+  //       trigger: this.imageSecond.nativeElement,
+  //       scrub: true,
+  //       start: '80% center'
+  //     },
+  //     duration: 1.1,
+  //     scale: 1.2,
+  //     height: 380,
+  //   });
+  //   gsap.to(this.document.querySelector('.heading-1'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('.heading-1'),
+  //       scrub: true,
+  //       start: '150% center',
+  //     },
+  //     color: '#fff',
+  //     duration: 1.5,
+  //   });
+  //   gsap.to(this.document.querySelector('.paragraph'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('.paragraph'),
+  //       scrub: true,
+  //       start: '150% center',
+  //     },
+  //     color: '#fff',
+  //     duration: 1.5
+  //   });
+  //   gsap.to(this.document.querySelector('.btn'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('btn'),
+  //       scrub: true,
+  //       start: '150% center',
+  //     },
+  //     color: '#fff',
+  //     duration: 1.5
+  //   });
+  //   gsap.from(this.document.querySelector('#buy'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('#buy'),
+  //       scrub: true,
+  //       toggleClass: 'active',
+  //       start: 'top center',
+  //     },
+  //     duration: 1.5,
+  //     y: 40,
+  //     opacity: 0,
+  //   });
+  //   gsap.from(this.document.querySelector('#about'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('#about'),
+  //       scrub: true,
+  //       toggleClass: 'active',
+  //       start: 'top center',
+  //     },
+  //     duration: 1.5,
+  //     y: 40,
+  //     opacity: 0,
+  //   });
+  //   gsap.from(this.document.querySelector('.box'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('.box'),
+  //       scrub: true,
+  //       toggleClass: 'active',
+  //       start: '-10% center',
+  //     },
+  //     duration: 1.5,
+  //     width: 0,
+  //     opacity: 0,
+  //   });
+  //   gsap.from(this.document.querySelector('.info-1__visual img'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('.info-1__visual img'),
+  //       scrub: true,
+  //       toggleClass: 'active',
+  //       start: '-60% bottom',
+  //     },
+  //     duration: 1.5,
+  //     width: 0,
+  //     opacity: 0,
+  //   });
+  //   gsap.from(this.document.querySelector('.quote'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('.quote'),
+  //       scrub: true,
+  //       toggleClass: 'active',
+  //       start: '-60% bottom',
+  //     },
+  //     duration: 1.5,
+  //     opacity: 0,
+  //   });
+  //   gsap.from(this.document.querySelector('.info-1__visual .heading-3'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('.info-1__visual .heading-3'),
+  //       scrub: true,
+  //       toggleClass: 'active',
+  //       start: '-60% bottom',
+  //     },
+  //     duration: 1.5,
+  //     y: 40,
+  //     opacity: 0,
+  //   });
+  //   gsap.from(this.document.querySelector('.info-1__visual .btn--learn'), {
+  //     scrollTrigger: {
+  //       trigger: this.document.querySelector('.info-1__visual .btn--learn'),
+  //       scrub: true,
+  //       toggleClass: 'active',
+  //       start: '-60% bottom',
+  //     },
+  //     duration: 1.5,
+  //     y: 40,
+  //     opacity: 0,
+  //   });
+  // }
 
-  initialAnimations(): void{
-    gsap.from(this.menu.nativeElement.childNodes, {
-      duration: 0.5,
-      opacity: 0,
-      y: -20,
-      stagger: 0.2,
-      delay: 0.5,
-    });
-    gsap.from(this.menuSecond.nativeElement.childNodes, {
-      duration: 0.5,
-      opacity: 0,
-      y: -20,
-      stagger: 0.2,
-      delay: 0.8,
-    });
-    gsap.from(this.imageFirst.nativeElement.childNodes, {
-      duration: 0.7,
-      opacity: 0,
-      y: -30,
-      stagger: 0.2,
-      delay: 0.5,
-    });
-    gsap.from(this.imageSecond.nativeElement.childNodes, {
-      duration: 0.7,
-      opacity: 0,
-      y: -30,
-      stagger: 0.2,
-      delay: 0.6,
-    });
-    gsap.from(this.document.querySelector('.heading-1'), {
-      duration: 0.7,
-      opacity: 0,
-      y: -30,
-      stagger: 0.2,
-      delay: 0.6,
-    });
-    gsap.from(this.document.querySelector('.paragraph'), {
-      duration: 0.7,
-      opacity: 0,
-      y: -30,
-      stagger: 0.2,
-      delay: 0.7,
-    });
-    gsap.from(this.document.querySelector('.btn'), {
-      duration: 0.7,
-      opacity: 0,
-      y: -30,
-      stagger: 0.2,
-      delay: 0.8,
-    });
-  }
+  // initialAnimations(): void{
+  //   gsap.from(this.menu.nativeElement.childNodes, {
+  //     duration: 0.5,
+  //     opacity: 0,
+  //     y: -20,
+  //     stagger: 0.2,
+  //     delay: 0.5,
+  //   });
+  //   gsap.from(this.menuSecond.nativeElement.childNodes, {
+  //     duration: 0.5,
+  //     opacity: 0,
+  //     y: -20,
+  //     stagger: 0.2,
+  //     delay: 0.8,
+  //   });
+  //   gsap.from(this.imageFirst.nativeElement.childNodes, {
+  //     duration: 0.7,
+  //     opacity: 0,
+  //     y: -30,
+  //     stagger: 0.2,
+  //     delay: 0.5,
+  //   });
+  //   gsap.from(this.imageSecond.nativeElement.childNodes, {
+  //     duration: 0.7,
+  //     opacity: 0,
+  //     y: -30,
+  //     stagger: 0.2,
+  //     delay: 0.6,
+  //   });
+  //   gsap.from(this.document.querySelector('.heading-1'), {
+  //     duration: 0.7,
+  //     opacity: 0,
+  //     y: -30,
+  //     stagger: 0.2,
+  //     delay: 0.6,
+  //   });
+  //   gsap.from(this.document.querySelector('.paragraph'), {
+  //     duration: 0.7,
+  //     opacity: 0,
+  //     y: -30,
+  //     stagger: 0.2,
+  //     delay: 0.7,
+  //   });
+  //   gsap.from(this.document.querySelector('.btn'), {
+  //     duration: 0.7,
+  //     opacity: 0,
+  //     y: -30,
+  //     stagger: 0.2,
+  //     delay: 0.8,
+  //   });
+  // }
 }
