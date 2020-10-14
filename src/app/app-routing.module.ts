93ef415b2dfc9b0aa31c309e549256e857ab47c1
Desiree Gamez <desireegamezinfantes@gmail.com>
2020-10-14 15:01:34 +0200

    actualizando rama desi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { PerfilAjenoComponent } from './pages/perfil-ajeno/perfil-ajeno.component';
 import { PerfilPropioComponent } from './pages/perfil-propio/perfil-propio.component';
+import { PublicarClaseComponent } from './pages/publicar-clase/publicar-clase.component';
 
 const routes: Routes =
 [
   {path: "", redirectTo: "/landing-page", pathMatch:'full'},
   {path: "landing-page", component: LandingPageComponent},
   {path: "home", component: HomeComponent},
-  {path: "perfil", component: PerfilPropioComponent}
+  {path: "perfil", component: PerfilPropioComponent},
+  {path: "user", component: PerfilAjenoComponent},
+  {path: "publicar", component: PublicarClaseComponent}
+
 ];
 
 @NgModule({
