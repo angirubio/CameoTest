2b529cba0823aa406a6e2812a7a4b995a0f5d3f7
pipe473 <bedoyafelipe743@gmail.com>
2020-10-19 15:33:47 +0200

    Añadiendo validaciones al registro

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario';
 import { UsuarioService } from 'src/app/shared/usuario.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -9,12 +10,37 @@ import { UsuarioService } from 'src/app/shared/usuario.service';
 })
 export class RegistroComponent implements OnInit {
 
-  public default_foto:string = "https://cdn2.vectorstock.com/i/thumb-large/23/81/default-avatar-profile-icon-vector-18942381.jpg";
-  constructor(private apiService: UsuarioService) { }
+  public myForm: FormGroup;
 
-  insertarUsuario(nombre:string, apellido:string, email:string, nombre_usuario:string, contrasena:string)
+  public default_foto:string = "https://cdn2.vectorstock.com/i/thumb-large/23/81/default-avatar-profile-icon-vector-18942381.jpg";
+  constructor(private apiService: UsuarioService, private formBuilder: FormBuilder) { 
+    this.buildForm();
+  }
+
+  public insertarUsuario(){
+    let usuario = this.myForm.value;
+    this.insertUser(usuario); 
+    console.log(usuario);
+          
+  }
+
+  private buildForm()
+  {
+    const minPassLength = 8;
+    const maxPassLength = 20;
+
+    this.myForm = this.formBuilder.group({
+      nombre: [, Validators.required],
+      apellido: [, Validators.required],
+      nombre_usuario: [, Validators.required],
+      email: [, [Validators.required, Validators.email]],
+      contrasena: [, [Validators.required, Validators.minLength(minPassLength), Validators.maxLength(maxPassLength)]],
+    });
+  }
+
+  insertUser(user:any)
   { 
-    this.apiService.postUsuario(new Usuario(0,nombre,apellido, nombre_usuario, email, contrasena,this.default_foto)).subscribe((data) =>
+    this.apiService.postUsuario(new Usuario(0,user.nombre, user.apellido, user.nombre_usuario, user.email, user.contrasena, this.default_foto)).subscribe((data) =>
     {
       console.log(data);
     })
