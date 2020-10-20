0c1f028662fe3b006803794661ee8c2eb87005cf
pipe473 <bedoyafelipe743@gmail.com>
2020-10-20 13:11:00 +0200

    creando nuevo formulario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 })
 export class RegistroComponent implements OnInit {
 
+  // Creo un objeto myForm de tipo predefinido FormGroup de angular
   public myForm: FormGroup;
 
   public default_foto:string = "https://cdn2.vectorstock.com/i/thumb-large/23/81/default-avatar-profile-icon-vector-18942381.jpg";
@@ -18,12 +19,14 @@ export class RegistroComponent implements OnInit {
     this.buildForm();
   }
 
-  // Este sera el onClick
+  // Este será el evento que ejecutará onClick
   public insertarUsuario(){
     let usuario = this.myForm.value;
     this.insertUser(usuario);
   }
 
+
+  // Constructor de las validaciones
   private buildForm()
   {
     const minPassLength = 8;
@@ -31,6 +34,7 @@ export class RegistroComponent implements OnInit {
     const minWordsLength = 4;
     const maxWordsLength = 30;
 
+    // Guardo las propiedades que se recogen del formulario con el metodo de angular formBuilder.group
     this.myForm = this.formBuilder.group({
       nombre: [, Validators.maxLength(maxWordsLength)],
       apellido: [, Validators.maxLength(maxWordsLength)],
@@ -40,13 +44,16 @@ export class RegistroComponent implements OnInit {
     });
   }
 
+  // Metodo que llama al servicio para crear el registro 
   insertUser(user:any)
   { 
-    this.apiService.postUsuario(new Usuario(0,user.nombre, user.apellido, user.nombre_usuario, user.email, user.contrasena, this.default_foto)).subscribe((data) =>
+    this.apiService.postUsuario(new Usuario(0, user.nombre, user.apellido, user.nombre_usuario, user.email, user.contrasena, this.default_foto)).subscribe((data) =>
     {
       console.log(data);
+
+      // metodo definido de angular para que cuando se cumpla la petición redirija a la home
       this.router.navigateByUrl('/home')
-    })
+    });
   }
 
   ngOnInit(): void {
