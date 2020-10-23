8917ae8a962c2b71897e94aa27e033b1c6023550
Desiree Gamez <desireegamezinfantes@gmail.com>
2020-10-23 12:45:09 +0200

    últimos cambios en componente organizar clases

--- src/app/pages/organizar-clases/organizar-clases.component.ts
+++ src/app/pages/organizar-clases/organizar-clases.component.ts
@@ -1,27 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-organizar-clases',
   templateUrl: './organizar-clases.component.html',
   styleUrls: ['./organizar-clases.component.css']
 })
 export class OrganizarClasesComponent implements OnInit {
-
   public claseCssPublicada: boolean;
-
+  public estaPublicada: boolean;
   constructor() {
     this.claseCssPublicada = false;
+    this.estaPublicada = false; 
   }
-
-  public cambiarEstadoConfirmar():void
+  public cambiarEstadoClase():void
   { 
     this.claseCssPublicada = !this.claseCssPublicada
-    
+    this.estaPublicada = !this.estaPublicada
   }
-
-
-
   ngOnInit(): void {
   }
-
 }
