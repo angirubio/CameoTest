5fcafcd5a83795d39faef27eb38e30fc1a8d175f
Angi Rubio <rubio.mailbox@gmail.com>
2020-10-27 16:49:47 +0100

    publicada cambio

--- src/app/pages/publicar-clase/publicar-clase.component.ts
+++ src/app/pages/publicar-clase/publicar-clase.component.ts
@@ -17,6 +17,7 @@ export class PublicarClaseComponent implements OnInit {
 
   public default_foto_clase:string = "https://res.cloudinary.com/rebelwalls/image/upload/b_black,c_fill,f_auto,fl_progressive,h_533,q_auto,w_800/v1428566112/article/R11191_image1";
   public id:number = this.apiService.usuario.usuario_id;
+  public publicada:boolean = true;
   constructor(private apiService:UsuarioService, private claseService:ClasesService, private router: Router) { }
 
   // Botones para ir a siguiente
@@ -39,7 +40,7 @@ export class PublicarClaseComponent implements OnInit {
     if (foto == ""){
       this.default_foto_clase = foto;
     }
-    this.claseService.clase = new Clases(0,titulo,descripcion,precio, tema, habilidad, fecha, plataforma,foto,this.id)
+    this.claseService.clase = new Clases(0,titulo,descripcion,precio, tema, habilidad, fecha, plataforma,foto,this.id,this.publicada)
     this.claseService.postClase(this.claseService.clase).subscribe((data) =>
     {
       this.claseService.clase = data[0];
