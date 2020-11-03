d9a79e3dfd84590073e21b83f87d0235b33937ac
pipe473 <bedoyafelipe743@gmail.com>
2020-11-03 21:30:12 +0100

    añadiendo validaciones al registro en el repetir contraseña

--- src/app/pages/registro/registro.validators.ts
+++ src/app/pages/registro/registro.validators.ts
@@ -1,12 +1,12 @@
-import { FormGroup, ValidationErrors, ValidatorFn } from "@angular/forms"
+import { FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export const validarQueSeanIguales: ValidatorFn = (
   control: FormGroup
 ): ValidationErrors | null => {
-  const password = control.get("contrasena")
-  const confirmarPassword = control.get("repetir_password")
+  const password = control.get('contrasena');
+  const confirmarPassword = control.get('repetir_password');
 
   return password.value === confirmarPassword.value
     ? null
-    : { noSonIguales: true }
-}
+    : { noSonIguales: true };
+};
